a299151ada5455cf576d3e34efc1e075a020e5df
Venkata Koripalli <venkorip2@WKMZTD73A06D.global.publicisgroupe.net>
2022-02-04 12:16:44 +0000

    refactor the ui packages

--- src/test/java/com/hsbc/qe/ui/pageobjects/LoginPage.java
+++ src/test/java/com/hsbc/qe/ui/pageobjects/LoginPage.java
@@ -1,15 +1,12 @@
 package com.hsbc.qe.ui.pageobjects;
 
 import com.hsbc.qe.ui.browsermanager.BrowserUtilities;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.ArrayList;
-import java.util.List;
 
 public class LoginPage {
 
@@ -52,7 +49,4 @@ public class LoginPage {
         browser.waitForJQueryLoad();
         LOGGER.info("Login to the app.");
     }
-
-
-
 }
